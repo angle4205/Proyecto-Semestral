b54a193ae5ce0c885fbf44d2229a62544d7060d8
angle4205 <angomez4205@gmail.com>
2024-04-05 02:33:09 -0300

    Navbar ajustado y repositorio más ordenado

--- LoginScript.js
+++ LoginScript.js
@@ -1,6 +1,15 @@
+/* Variable del container*/
 const container = document.getElementById('container');
+/* Variable boton registrarse*/
 const registerBtn = document.getElementById('register');
+/* Variable boton Iniciar sesion*/
 const loginBtn = document.getElementById('login');
+
+/* 
+EventListener x2 para cada boton
+classList.add para añadir "active" a la clase html de container
+classList.remove para remover el mismo string de la misma clase
+*/
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");

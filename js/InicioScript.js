09787fef672f40134bf3c072e12a3f74acb03beb
angle4205 <angomez4205@gmail.com>
2024-05-06 23:45:41 -0400

    Diseño de contenedor API modificado

--- js/InicioScript.js
+++ js/InicioScript.js
@@ -1,45 +1,42 @@
 $(document).ready(function () {
-    $.ajax({
-        url: "https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39",
-        method: "GET",
-        headers: {
-            "x-rapidapi-key":
-                "9299052402mshfb49d105f14df47p1e56fdjsnca95cebafabe",
-            "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-        },
-        success: function (response) {
-            if (response.response && response.response.length > 0) {
-                var equipos = response.response[0].league.standings[0];
-                equipos.sort(function (a, b) {
-                    return b.points - a.points;
-                });
-                for (var i = 0; i < 3; i++) {
+  $.ajax({
+    url: "https://api-football-v1.p.rapidapi.com/v3/standings?season=2020&league=39",
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "9299052402mshfb49d105f14df47p1e56fdjsnca95cebafabe",
+      "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+    },
+    success: function (r) {
+      if (r.response && r.response.length > 0) {
+        var equipos = r.response[0].league.standings[0];
+        equipos.sort(function (a, b) {
+          return b.points - a.points;
+        });
+        for (var i = 0; i < 3; i++) {
+          console.log(equipos[i].team.name);
 
-
-                    console.log(equipos[i].team.name);
-
-                    $("#equiposPunteros").append(
-                        '<div class="card" style="width: 18rem; margin: 10px;">' +
-                        '<img src="' +
-                        equipos[i].team.logo +
-                        '" class="card-img-top" alt="' +
-                        equipos[i].team.name +
-                        '">' +
-                        '<div class="card-body">' +
-                        '<h5 class="card-title">' +
-                        equipos[i].team.name +
-                        "</h5>" +
-                        '<p class="card-text">Puntos: ' +
-                        equipos[i].points +
-                        "</p>" +
-                        "</div>" +
-                        "</div>"
-                    );
-                }
-            }
-        },
-        error: function (error) {
-            console.error("Error al cargar datos:", error);
-        },
-    });
+          $("#equiposPunteros").append(
+            $(
+              '<div class="container" style="width: 100px; margin: 10px; display: flex; flex-direction: column; align-items: center; justify-content: center;">'
+            )
+              .append(
+                '<img src="' +
+                  equipos[i].team.logo +
+                  '" class="card-img-top" alt="' +
+                  equipos[i].team.name +
+                  '">'
+              )
+              .append(
+                '<p class="card-text" style="margin-top: 10px;">' +
+                  equipos[i].points +
+                  " pts</p>"
+              )
+          );
+        }
+      }
+    },
+    error: function (error) {
+      console.error("Error:", error);
+    },
+  });
 });
